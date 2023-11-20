f55352f8ab76849b1c2475b44313be0dbd7f8c30
Jhonny Asto <133028636+JhonnyASTOatoccsa@users.noreply.github.com>
2023-11-20 10:27:58 -0500

    Update confign.py

--- interfaces_hds/calculadoraAvanzada/confign.py
+++ interfaces_hds/calculadoraAvanzada/confign.py
@@ -1,21 +1,22 @@
-#constantes para el tema oscuro
-color_fondo_negro="#1a1b28"
-color_texto_negro="white"
-color_botones_especial_negro="#52c9dc"
-color_botones_negro="#1e2435"
-color_caja_texto_negro="#1a1b28"
+# vamos a crear las constantes para el tema oscuro, no se modifican por eso ya no son variables, se le conoce como constante, cuando el dato es cambiante deja de ser una simple variable y debe ir en mayuscula.
+COLOR_FONDO_NEGRO='#1a1b28' # adobe color o color picker para el codigo de colores
+COLOR_TEXTO_NEGRO='white'
+COLOR_BOTONES_ESPECIAL_NEGRO="#52c9dc"
+COLOR_BOTON_NEGRO='#1e2435'
+COLOR_CAJA_TEXTO_NEGRO='#1a1b28'
 
-#constante para el tema claro
-color_fondo_light="#ffffff"
-color_fondo_light="black"
-color_botones_especial_light="#52c9dc"
-color_botones_ligth="#f2f8fc"
-color_caja_texto_light="#ffffff"
+# constantes para el tema claro 
+COLOR_FONDO_LIGHT='#ffffff' # adobe color o color picker para el codigo de colores
+COLOR_TEXTO_LIGHT='black'
+COLOR_BOTONES_ESPECIAL_LIGHT='#52c9dc'
+COLOR_BOTON_LIGHT='#f2f8fc'
+COLOR_CAJA_TEXTO_LIGHT='#ffffff'
 
-#funciones que nos permite centrar nuestra pantalla
+
+# funcion que nos permite centrar nuestra pantalla
 def centrar_ventana(ventana,ancho_ventana,largo_ventana):
-    pantalla_ancho=ventana.winfo_screenwidth()
-    pantalla_largo=ventana.winfo_screenheight()
+    pantalla_ancho=ventana.winfo_screenwidth() # quiero la referencia del ancho de mi ventana (toda la pantalla) almacena el ancho de la ventana padre
+    pantalla_largo=ventana.winfo_screenheight() # tenemos que dividir entre dos e igual lo que 
     x=int((pantalla_ancho/2)-(ancho_ventana/2))
-    y=int((pantalla_largo/2)-(ancho_ventana/2))
-    return ventana.geometry(f"{ancho_ventana}x{largo_ventana}+{x}+{y}")
+    y=int((pantalla_largo/2)-(largo_ventana/2))
+    return ventana.geometry (f'{ancho_ventana}x{pantalla_largo}+{x}+{y}')
