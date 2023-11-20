0b85d87123be9a3c051acc2bd6ce382ec86043c4
Jhonny Asto <133028636+JhonnyASTOatoccsa@users.noreply.github.com>
2023-11-20 10:58:42 -0500

    interfaz.py

--- interfaces_hds/calculadoraAvanzada/interfaz.py
+++ interfaces_hds/calculadoraAvanzada/interfaz.py
@@ -1,20 +1,15 @@
 from tkinter import *
 from tkinter import font
-import config as cons
+import confign as cons
 from funciones import *
-# importa el archuivo config y le asignas variable 
-## creaer una clase, llamada interfzas de calculadora
-
-## clase hija que va heredar todod oo que tenga tk
-
-class interfaz_calculadora(Tk):
+class InterfazCalculadora(Tk):
     def __init__(self):
-        super().__init__() # especificamos cual valor quiero que se ejecute
+        super().__init__()
         self.configura_ventana()
         self.construir_widget()
         self.boton_cambio_tema()
 
-        # crear metodos
+        
     def configura_ventana(self):
         self.title("Calculadora avanzada")
         #color
@@ -28,15 +23,18 @@ class interfaz_calculadora(Tk):
         cons.centrar_ventana(self,w,h)
 
     def construir_widget(self):
-        #etiqueta para mostrar la operacion
-        self.operacion_label=Label(self,text="",font=("Arial",16),fg=cons.COLOR_TEXTO_NEGRO,bg=cons.COLOR_FONDO_NEGRO,justify="right")
+        
+        self.operacion_label=Label(self,text="",font=("Arial",16),
+        fg=cons.COLOR_TEXTO_NEGRO,bg=cons.COLOR_FONDO_NEGRO,justify="right")
         self.operacion_label.grid(row=0,column=3,padx=10,pady=10)
 
-        #caja de texto donde se muestra los numero ingresado
-        self.caja_operaciones=Entry(self,width=12,font=("Arial",40),bd=0,fg=cons.COLOR_TEXTO_NEGRO,bg=cons.COLOR_FONDO_NEGRO,justify="right")
+       
+        self.caja_operaciones=Entry(self,width=12,
+        font=("Arial",40),bd=0,fg=cons.COLOR_TEXTO_NEGRO,
+        bg=cons.COLOR_FONDO_NEGRO,justify="right")
         self.caja_operaciones.grid(row=1,column=0,columnspan=4,padx=10,pady=10)
 
-        #creado botones
+        
         botones=[
             "C","%","<","/",
             "7","8","9","*",
@@ -56,10 +54,17 @@ class interfaz_calculadora(Tk):
                 color_fondo=cons.COLOR_BOTON_NEGRO
                 boton_font=robot_font
             if boton=="=":
-                Button(self,command=lambda b=boton:enviar_boton(self,b),text=boton,width=11,height=2,bg=color_fondo,fg=cons.COLOR_TEXTO_NEGRO,relief=FLAT,font=boton_font,padx=5,pady=5,bd=0,borderwidth=0,highlightthickness=0,overrelief="flat").grid(row=row_ini,column=col_ini,columnspan=2,pady=5)
+                Button(self,command=lambda b=boton:enviar_boton(self,b),
+                text=boton,width=11,height=2,bg=color_fondo,fg=cons.COLOR_TEXTO_NEGRO,
+                relief=FLAT,font=boton_font,padx=5,pady=5,bd=0,borderwidth=0,
+                highlightthickness=0,overrelief="flat").grid(row=row_ini,column=col_ini,
+                columnspan=2,pady=5)
                 col_ini +=1
             else:
-                Button(self,command=lambda b=boton:enviar_boton(self,b),text=boton,width=5,height=2,bg=color_fondo,fg=cons.COLOR_TEXTO_NEGRO,relief=FLAT,font=boton_font,padx=5,pady=5,bd=0,borderwidth=0,highlightthickness=0,overrelief="flat").grid(row=row_ini,column=col_ini,pady=5)
+                Button(self,command=lambda b=boton:enviar_boton(self,b),text=boton,
+                width=5,height=2,bg=color_fondo,fg=cons.COLOR_TEXTO_NEGRO,relief=FLAT,
+                font=boton_font,padx=5,pady=5,bd=0,borderwidth=0,highlightthickness=0,
+                overrelief="flat").grid(row=row_ini,column=col_ini,pady=5)
                 col_ini +=1
             if col_ini>3:
                 col_ini=0
@@ -68,7 +73,9 @@ class interfaz_calculadora(Tk):
     def boton_cambio_tema(self):
         self.tema_oscuro=True
         font_icono=font.Font(family='FontAwesone',size=12)
-        self.boton_tema=Button(self,text='Modo Oscuro \uf136',width=13,font=font_icono,bd=0,borderwidth=0,highlightthickness=0,relief=FLAT,bg=cons.COLOR_BOTONES_ESPECIAL_LIGHT,command=lambda:cambio_tema(self,cons))
+        self.boton_tema=Button(self,text='Modo Oscuro \uf136',width=13,font=font_icono,
+        bd=0,borderwidth=0,highlightthickness=0,relief=FLAT,
+        bg=cons.COLOR_BOTONES_ESPECIAL_LIGHT,command=lambda:cambio_tema(self,cons))
         self.boton_tema.grid(row=0,column=0,columnspan=2,padx=0,pady=0,sticky='nw')
     
 
