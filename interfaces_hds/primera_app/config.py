7358957c6203752fef90ce14aee8b5db29e39e86
Jhonny Asto <133028636+JhonnyASTOatoccsa@users.noreply.github.com>
2023-11-24 08:13:34 -0500

    ss config.py

--- interfaces_hds/primera_app/config.py
+++ interfaces_hds/primera_app/config.py
@@ -1,16 +1,17 @@
 import datetime
-color_fondo_primario="#004d4d"
-color_fondo_secundario="#b3daff"
-color_boton="#52c9dc"
-titulo_app="app de contactos"
+COLOR_FONDO_PRIMARIO="#004d4d"
+COLOR_FONDO_SECUNDARIO="#b3daff"
+COLOR_BOTONES="#52c9dc"
+
+TITULO_APP="App de contactos"
 date=datetime.datetime.now()
-hora_actual=f"fecha: {date.day}-{date.month}-{date.year},hora: {date.hour}:{date.minute}"
+HORA_ACTUAL=f"                       Puquio,   {date.day}/{date.month}/{date.year},  hora:{date.hour}:{date.minute}"
 
 def centrar_ventana(ventana,ancho_ventana,largo_ventana):
     pantalla_ancho=ventana.winfo_screenwidth()
-    pantalla_largo=ventana.winfo_screenheigth()
+    pantalla_largo=ventana.winfo_screenheight()
+
     x=int((pantalla_ancho/2)-(ancho_ventana/2))
     y=int((pantalla_largo/2)-(largo_ventana/2))
-    return ventana.geometry(f"{ancho_ventana}x{largo_ventana}+{x}+{y}")
 
-  
+    return ventana.geometry(f"{ancho_ventana}x{largo_ventana}+{x}+{y}")
